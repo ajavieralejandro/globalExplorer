f177fd2afddb23dbfe0af7b051958745a38ae768
ajavieralejandro <amorosijavier@gmail.com>
2025-09-29 14:09:34 -0300

    Addapting languagge

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+// Seteá VITE_PUBLIC_BASE="/tu-repo/" en el workflow de GH Actions (o hardcodeá abajo)
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build'
+  const publicBase = process.env.VITE_PUBLIC_BASE || '/' // p/ dev y dominios propios
+  return {
+    plugins: [react(), tailwindcss()],
+    base: isBuild ? publicBase : '/', // en dev siempre '/'
+  }
 })
